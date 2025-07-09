06b22106fb4d1f73ec89ccc3371057cc49682469
AAbukhdair <aabukhdair@mail.valenciacollege.edu>
2025-07-09 14:47:18 -0400

    Initial Commit

---
+++ QuranTracer/QuranTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranTracerApp.swift
+//  QuranTracer
+//
+//  Created by Adam Abukhdair on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct QuranTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuranTracerTests/QuranTracerTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuranTracerTests.swift
+//  QuranTracerTests
+//
+//  Created by Adam Abukhdair on 7/9/25.
+//
+
+import Testing
+@testable import QuranTracer
+
+struct QuranTracerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
